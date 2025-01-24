4c452f2a603f2dae5ccfdb835162692e94db941a
Vangala Aruna <arunajava567@gmail.com>
2025-01-24 15:48:12 +0530

    Add files via upload

--- jdbc/JdbcApp.java
+++ jdbc/JdbcApp.java
@@ -18,7 +18,9 @@ public class JdbcApp {
 		
 	//	st.executeUpdate("create table hospital(id int primary key ,name varchar(20)) ");
 	//	System.out.println(" table created ");
-		st.execute("insert into hospital values(5,'Medicover')");
+		st.execute("insert into hospital values(51,'Medicover')");
+		st.execute("insert into hospital values(6,'Medicover78')");
+		st.execute("insert into hospital values(7,'Medicover56')");
 	//	st.executeUpdate("update hospital set name='AIG' where id=2");
 	//	st.executeUpdate("delete from  hospital where id=3");
 		System.out.println("inserted...");
